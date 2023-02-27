8aae45070936967c44d3c8232001793a0b664494
Art <artem.nii@gmail.com>
2023-02-27 23:22:56 +0300

    buy version 2

--- cypress/e2e/finder.cy.js
+++ cypress/e2e/finder.cy.js
@@ -9,31 +9,12 @@ import basePage from '../support/pages/BasePage'
 describe('Fine Finder', () => {
     it('Find my goods', () => {
 
-
-        loginSilent(user)
-
-        cy.visit('/index.php?rt=account/account')
-        cy.get('.menu_text').should('contain', `Welcome back ${user.firstName}`)
-        cy.get('#filter_keyword').type('e {ENTER}')
-
-        productFinder('ck one Summer 3.4 oz')
-
-        cy.get('.productpagecart .cart').click()
-        cy.get('.block_7 .dropdown.hover .font14').should('contain', '1')
-        cy.get('.block_7 .dropdown.hover .cart_total').should('contain', '$27.00')
-        cy.get('#cart_checkout1').click()
-        cy.get('#checkout_btn').click()
-        cy.contains(' Your Order Has Been Processed!')
-        cy.url().should('eq', 'https://automationteststore.com/index.php?rt=checkout/success')
-/*
         loginSilent(user)
         cy.visit('/index.php?rt=account/account')
         basePage.checkIfTopNavHaveUserName(user)
         basePage.findNeededValue('e')
 
-
         productFinder('ck one Summer 3.4 oz')
-
 
         productPage.clickAddToCartButton()
         productPage.checkIfOnePosition()
@@ -41,6 +22,6 @@ describe('Fine Finder', () => {
         productPage.clickCartCheckout()
         productPage.clickAddToCartButton()
         productPage.checkIfBought()
-        */
+
     })
 })
